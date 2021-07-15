c89e97c36dd4ae375bac265d3dfb9b0982a76653
BuildTools <unconfigured@null.spigotmc.org>
2021-07-15 18:45:47 +0300

    Update database upgrade error message
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/me/william278/huskhomes2/migrators/UpgradeDatabase.java
+++ src/main/java/me/william278/huskhomes2/migrators/UpgradeDatabase.java
@@ -17,15 +17,15 @@ public class UpgradeDatabase {
         if (plugin.getConfig().getInt("config_file_version", 1) <= 5) {
             plugin.getLogger().info("Detected that the database needs updating. Running database upgrade...");
             Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-                try (PreparedStatement statement = HuskHomes.getConnection().prepareStatement(
+                try (PreparedStatement tableUpdateStatement = HuskHomes.getConnection().prepareStatement(
                         "ALTER TABLE " + HuskHomes.getSettings().getPlayerDataTable()
                                 + " ADD `is_ignoring_requests` boolean NOT NULL DEFAULT 0, "
                                 + "`offline_location_id` integer NULL DEFAULT NULL, "
                                 + "FOREIGN KEY (`offline_location_id`) REFERENCES " + HuskHomes.getSettings().getLocationsDataTable() + " (`location_id`) ON DELETE SET NULL ON UPDATE NO ACTION;")) {
-                    statement.executeUpdate();
+                    tableUpdateStatement.executeUpdate();
                     plugin.getLogger().info("Database update complete!");
                 } catch (SQLException e) {
-                    plugin.getLogger().log(Level.SEVERE, "An SQL exception occurred upgrading the database format!", e);
+                    plugin.getLogger().warning("Failed to perform the database upgrade: " + e.getCause() + ". This might be because another server on your HuskHomes network already carried out the upgrade - in which case you can safely ignore this warning.");
                 } finally {
                     // Update the config file version
                     Bukkit.getScheduler().runTask(plugin, () -> {

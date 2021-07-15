3c14856db65ad3beff01bd364c1256c22ba44d22
BuildTools <unconfigured@null.spigotmc.org>
2021-07-15 18:00:31 +0300

    Update translators section of plugin information
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/me/william278/huskhomes2/commands/HuskHomesCommand.java
+++ src/main/java/me/william278/huskhomes2/commands/HuskHomesCommand.java
@@ -25,7 +25,7 @@ public class HuskHomesCommand extends CommandBase implements TabCompleter {
             .append("[").append(plugin.getDescription().getDescription()).append("](gray)\n")
             .append("[• Author:](white) [William278](gray show_text=&7Click to pay a visit open_url=https://youtube.com/William27528)\n")
             .append("[• Contributors:](white) [imDaniX](gray show_text=&7Code, refactoring), [Log1x](gray show_text=&7Code)\n")
-            .append("[• Translators:](white) [RohFrenzy](gray show_text=&7German Translation, de-de), [咖波](gray show_text=&7Chinese Translation, zh-tw), [imDaniX](gray show_text=&7Russian Translation, ru), [ReferTV](gray show_text=&7Polish Translation, pl), [Villag3r_](gray show_text=&7Italian Translation, it-it) \n")
+            .append("[• Translators:](white) [SnivyJ](gray show_text=&7Simplified Chinese, zh-cn), [Villag3r_](gray show_text=&7Italian, it-it) \n")
             .append("[• Help Wiki:](white) [[Link]](#00fb9a show_text=&7Click to open link open_url=https://github.com/WiIIiam278/HuskHomes2/wiki/)\n")
             .append("[• Report Issues:](white) [[Link]](#00fb9a show_text=&7Click to open link open_url=https://github.com/WiIIiam278/HuskHomes2/issues)\n")
             .append("[• Support Discord:](white) [[Link]](#00fb9a show_text=&7Click to join open_url=https://discord.gg/tVYhJfyDWG)");

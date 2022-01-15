2dd0bbb9d89d37b8b8047a4d480c84038cb438f9
William <will27528@gmail.com>
2022-01-15 19:10:23 +0000

    Send error if not enough players to do /tpall

--- src/main/java/me/william278/huskhomes2/commands/TpAllCommand.java
+++ src/main/java/me/william278/huskhomes2/commands/TpAllCommand.java
@@ -1,5 +1,6 @@
 package me.william278.huskhomes2.commands;
 
+import me.william278.huskhomes2.HuskHomes;
 import me.william278.huskhomes2.MessageManager;
 import me.william278.huskhomes2.teleport.TeleportManager;
 import org.bukkit.command.Command;
@@ -9,6 +10,10 @@ public class TpAllCommand extends CommandBase {
 
     @Override
     protected void onCommand(Player p, Command command, String label, String[] args) {
+        if (HuskHomes.getPlayerList().getPlayers().size() <= 1) {
+            MessageManager.sendMessage(p, "error_no_other_online_players");
+            return;
+        }
         TeleportManager.teleportAllHere(p);
         MessageManager.sendMessage(p, "teleporting_all_players");
     }

0d475cf4c766c0b1f1becf2e5aa1326e18e2879b
William <will27528@gmail.com>
2023-04-14 22:51:57 +0100

    sponge: Port HuskHomes to Sponge API v8.1.0

---
+++ sponge/src/main/java/net/william278/huskhomes/event/SpongeHomeDeleteEvent.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of HuskHomes, licensed under the Apache License 2.0.
+ *
+ *  Copyright (c) William278 <will27528@gmail.com>
+ *  Copyright (c) contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.william278.huskhomes.event;
+
+import net.william278.huskhomes.position.Home;
+import net.william278.huskhomes.user.CommandUser;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.api.event.Cancellable;
+import org.spongepowered.api.event.Cause;
+import org.spongepowered.api.event.Event;
+
+public class SpongeHomeDeleteEvent implements IHomeDeleteEvent, Event, Cancellable {
+
+    private boolean cancelled = false;
+    private final Home home;
+    private final CommandUser deleter;
+
+    public SpongeHomeDeleteEvent(@NotNull Home home, @NotNull CommandUser deleter) {
+        this.home = home;
+        this.deleter = deleter;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @NotNull
+    @Override
+    public Home getHome() {
+        return home;
+    }
+
+    @NotNull
+    @Override
+    public CommandUser getDeleter() {
+        return deleter;
+    }
+
+    @Override
+    public Cause cause() {
+        return Cause.builder()
+                .append(deleter)
+                .build();
+    }
+}
---
+++ sponge/src/main/java/net/william278/huskhomes/event/SpongeWarpListEvent.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of HuskHomes, licensed under the Apache License 2.0.
+ *
+ *  Copyright (c) William278 <will27528@gmail.com>
+ *  Copyright (c) contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.william278.huskhomes.event;
+
+import net.william278.huskhomes.position.Warp;
+import net.william278.huskhomes.user.CommandUser;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.api.event.Cancellable;
+import org.spongepowered.api.event.Cause;
+import org.spongepowered.api.event.Event;
+
+import java.util.List;
+
+public class SpongeWarpListEvent implements IWarpListEvent, Event, Cancellable {
+
+    private boolean cancelled = false;
+    private List<Warp> warps;
+    private final CommandUser listViewer;
+
+    public SpongeWarpListEvent(@NotNull List<Warp> warps, @NotNull CommandUser listViewer) {
+        this.warps = warps;
+        this.listViewer = listViewer;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @NotNull
+    @Override
+    public List<Warp> getWarps() {
+        return warps;
+    }
+
+    @Override
+    public void setWarps(@NotNull List<Warp> warps) {
+        this.warps = warps;
+    }
+
+    @NotNull
+    @Override
+    public CommandUser getListViewer() {
+        return listViewer;
+    }
+
+    @Override
+    public Cause cause() {
+        return Cause.builder()
+                .append(listViewer)
+                .build();
+    }
+
+}

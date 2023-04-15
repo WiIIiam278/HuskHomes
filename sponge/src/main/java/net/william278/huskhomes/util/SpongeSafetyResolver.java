2066a8971772e20a540ce2dd6529f60ad5d2a8bc
William <will27528@gmail.com>
2023-04-15 16:09:27 +0100

    sponge: Add ground safety resolver implementation, tweak workflows

--- sponge/src/main/java/net/william278/huskhomes/util/SpongeSafetyResolver.java
+++ sponge/src/main/java/net/william278/huskhomes/util/SpongeSafetyResolver.java
@@ -21,6 +21,10 @@ package net.william278.huskhomes.util;
 
 import net.william278.huskhomes.position.Location;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.api.block.BlockState;
+import org.spongepowered.api.registry.RegistryTypes;
+import org.spongepowered.api.world.server.ServerLocation;
+import org.spongepowered.math.vector.Vector3i;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -29,7 +33,28 @@ public interface SpongeSafetyResolver extends SafetyResolver {
 
     @Override
     default CompletableFuture<Optional<Location>> findSafeGroundLocation(@NotNull Location location) {
-        return CompletableFuture.completedFuture(Optional.of(location)); // todo
+        final Optional<ServerLocation> adaptedLocation = SpongeAdapter.adaptLocation(location);
+        if (adaptedLocation.isEmpty()) {
+            return CompletableFuture.completedFuture(Optional.empty());
+        }
+
+        final ServerLocation serverLocation = adaptedLocation.get();
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                final Vector3i cursor = serverLocation.asHighestLocation().blockPosition().add(x, 0, z);
+                final BlockState blockState = serverLocation.world().block(cursor);
+                if (isBlockSafe(blockState.type().key(RegistryTypes.BLOCK_TYPE).asString())) {
+                    return CompletableFuture.completedFuture(Optional.of(Location.at(
+                            cursor.x() + 0.5,
+                            cursor.y() + 1,
+                            cursor.z() + 0.5,
+                            location.getWorld()
+                    )));
+                }
+            }
+        }
+
+        return CompletableFuture.completedFuture(Optional.empty());
     }
 
 }
